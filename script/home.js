1bf638ef1c1575e632f8c87058f1b1fa220dd624
raselbabu273 <raselbabu273@gmail.com>
2026-03-16 03:39:12 +0600

    Active buttons, Issue number, border color design and dynamic done

--- script/home.js
+++ script/home.js
@@ -1,22 +1,24 @@
 
 const createElements = (arr) => {
     const colors = ["bg-red-200", "bg-green-200", "bg-blue-200"];
-    const htmlElements = arr.map((el) => `<span class="bg-gray-100 rounded-full px-2 py-1">${el}</span>`);
+    const htmlElements = arr.map((el) => `<span class="btn btn-soft text-[12px] uppercase bg-gray-100 rounded-full px-3 py-1">${el}</span>`);
     return htmlElements.join(' ');
 };
-
-const loadCard = () => {
+let allIssues = [];
+// Fetch All Issue card
+const loadCard = async () => {
     const url = 'https://phi-lab-server.vercel.app/api/v1/lab/issues';
-    fetch(url)
-        .then((res) => res.json())
-        .then((json) => {
-            displayCard(json.data);
-        });
+    const res = await fetch(url);
+    const data = await res.json();
+    allIssues = data.data;
+    displayCard(allIssues);
 };
 
+// ---------Active Buttons--------
 const buttons = document.querySelectorAll('.filter-btn');
+const items = document.querySelector('.issues');
 buttons.forEach((button) => {
-    button.addEventListener('click', function() {
+    button.addEventListener('click', function () {
         buttons.forEach((btn) => {
             btn.classList.remove('active');
         });
@@ -39,15 +41,23 @@ buttons.forEach((button) => {
 //     "createdAt": "2024-01-15T10:30:00Z",
 //     "updatedAt": "2024-01-15T10:30:00Z"
 // }
+// Display All Issue card
 const displayCard = (infos) => {
     const cardContainer = document.getElementById('card-container');
+    const issueCount = document.getElementById("issue-count");
+    cardContainer.innerHTML = "";
 
+    // issue card number
+    issueCount.innerText = infos.length;
 
     infos.forEach((info) => {
         const cardDiv = document.createElement('div');
+
+        // Border color
+        const borderColor = info.status === "open" ? "border-green-600" : "border-purple-600";
         cardDiv.innerHTML = `
-        <div id="card" class="shadow-md rounded-md h-100% border-t-4 border-neutral">
-                <div class="border-b border-gray-300 py-5 space-y-4">
+        <div id="issue-card" class="shadow-md rounded-md border-t-5 h-[27rem] max-sm:h-[33rem] ${borderColor}">
+                <div class="border-b border-gray-300 py-8 space-y-4">
                     <div class="flex justify-between items-center px-5">
                         <div><img src="./assets/Open-Status.png" alt=""></div>
                         <div class="bg-gray-300 px-3 rounded-full">${info.priority}</div>
@@ -60,16 +70,35 @@ const displayCard = (infos) => {
                         ${createElements(info.labels)}
                     </div>
                 </div>
-                <div class="p-5 space-y-4">
+                <div class="p-4 space-y-4">
                     <p class="text-[#64748B]">#${info.id} ${info.author}</p>
-                    <p class="text-[#64748B]">${info.updatedAt}</p>
+                    <p class="text-[#64748B]">Updated: ${info.updatedAt}</p>
                 </div>
             </div>
         `;
-        // <div class="text-red-500 bg-red-100 rounded-full px-2"><i class="fa-solid fa-bug"></i> Bug</div>
-        //                 <div class="text-orange-400 bg-orange-100 rounded-full px-2"><i class="fa-regular fa-life-ring"></i>Help Wanted</div>
+
         cardContainer.append(cardDiv);
     });
 };
+
+const allBtn = document.getElementById("all-btn");
+const openBtn = document.getElementById("open-btn");
+const closedBtn = document.getElementById("closed-btn");
+
+allBtn.addEventListener("click", () => {
+    displayCard(allIssues);
+});
+
+openBtn.addEventListener("click", () => {
+    const openIssues = allIssues.filter(issue => issue.status === "open")
+    displayCard(openIssues);
+});
+
+closedBtn.addEventListener("click", () => {
+    const closedIssues = allIssues.filter(issue => issue.status === "closed")
+    displayCard(closedIssues);
+});
+
+
 
 loadCard();
