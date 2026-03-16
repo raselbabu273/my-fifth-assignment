0599c86d46c540429e3a9f69b215cfb4bffbfd3d
raselbabu273 <raselbabu273@gmail.com>
2026-03-16 13:06:07 +0600

    html file connected

--- script/sign-in.js
+++ script/sign-in.js
@@ -1,22 +1,22 @@
-document.getElementById('sign-in-btn').addEventListener('click', function(){
+document.getElementById('sign-in-btn').addEventListener('click', function () {
     // 1. get the mobile number input
     const inputUsername = document.getElementById('input-username');
     const username = inputUsername.value;
-    
-    
+
+
     // 2. get the pin input
     const inputPassword = document.getElementById('input-password');
     const password = inputPassword.value;
-    
-    
+
+
     // 3. match pin & mobile number
-    if(username === 'admin' && password === 'admin123'){
+    if (username === 'admin' && password === 'admin123') {
 
         // 3-1. true:::>> alert > homepage
         alert('Login Successful');
-        window.location.assign('/home.html');
+        // window.location.assign('/home.html');
     }
-    else{
+    else {
         // 3-2. false:::>> alert > return
         alert('Login Failed');
         return;
